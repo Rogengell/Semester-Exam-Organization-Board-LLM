54d777f99cf650151a5cf726c7d144aaae3e782f
Rogengell <niels.rogengell@gmail.com>
2025-05-02 13:33:33 +0200

    Initial Full Setup

---
+++ OrganizationBoard.Tests/ServiceTests/Test.cs
@@ -0,0 +1,27 @@
+﻿using Xunit.Abstractions;
+
+namespace OrganizationBoard.Tests;
+
+public class Test
+{
+    private readonly ITestOutputHelper _outputHelper;
+    
+    public Test(ITestOutputHelper testoutputHelper)
+    {
+        _outputHelper = testoutputHelper;
+
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        int a = 1;
+        int b = 2;
+        //Act
+        int c = a+b;
+        //Assert
+        _outputHelper.WriteLine(""+c);
+        Assert.Equal(3,c);
+    }
+}
