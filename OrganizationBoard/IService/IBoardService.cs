43889e0c5798dc3cff5941de51e0b00458742acd
Lars <Bruger@DESKTOP-DUN3DKK>
2025-05-14 16:02:40 +0200

    Fixing IBoardService

--- OrganizationBoard/IService/IBoardService.cs
+++ OrganizationBoard/IService/IBoardService.cs
@@ -1,20 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using OrganizationBoard.DTO;
-using OrganizationBoard.Model;
+using EFrameWork.Model;
 
 namespace OrganizationBoard.IService
 {
     public interface IBoardService
     {
         // Task Management
-        Task<OperationResponse<CardTask>> CreateTask(CardTask task, int userId); // Only Team Leader
-        Task<OperationResponse<CardTask>> GetTask(int taskId);  //All
-        Task<OperationResponse<CardTask>> UpdateTask(CardTask task, int userId); // Team Leader can update all, members can only update assigned to
+        Task<OperationResponse<EFrameWork.Model.Task>> CreateTask(EFrameWork.Model.Task task, int userId); // Only Team Leader
+        Task<OperationResponse<EFrameWork.Model.Task>> GetTask(int taskId);  //All
+        Task<OperationResponse<EFrameWork.Model.Task>> UpdateTask(EFrameWork.Model.Task task, int userId); // Team Leader can update all, members can only update assigned to
         Task<OperationResponse<bool>> DeleteTask(int taskId, int userId); // Only Team Leader
-        Task<OperationResponse<List<CardTask>>> GetTeamTasks(int teamId, int userId); // All in team can see.
+        Task<OperationResponse<List<EFrameWork.Model.Task>>> GetTeamTasks(int teamId, int userId); // All in team can see.
 
         Task<OperationResponse<bool>> AssignTask(int taskId, int userId, int assignedToUserId); // Team Leader and Members
         Task<OperationResponse<bool>> MarkTaskAsComplete(int taskId, int userId); // Member
