4b86d880ac6cfa2f7f52424cb3f898a82d7ab4e2
Lars <Bruger@DESKTOP-DUN3DKK>
2025-05-23 13:13:44 +0200

    Current progress

--- LLMExamAgents/config.py
+++ LLMExamAgents/config.py
@@ -2,9 +2,7 @@ LLM_CONFIG = {
     "config_list": [
         {
             "model": "llama3.1:8b",
-            "api_key": "vZTO5AQxbVq4Vzlm7eJ1jrqS3s2P3rmD",
             "api_type": "ollama",
-            "api_rate_limit": 0.25,
             "temperature": 0.0,
             "stream": False,
         }
