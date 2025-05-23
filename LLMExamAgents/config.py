87dcd676533cc56bbb7919d1d409d8f24c60d895
Lars <Bruger@DESKTOP-DUN3DKK>
2025-05-23 10:26:10 +0200

    Small setup

---
+++ LLMExamAgents/config.py
@@ -0,0 +1,12 @@
+LLM_CONFIG = {
+    "config_list": [
+        {
+            "model": "??",
+            "api_key": "vZTO5AQxbVq4Vzlm7eJ1jrqS3s2P3rmD",
+            "api_type": "mistral",
+            "api_rate_limit": 0.25,
+            "temperature": 0.0,
+            "stream": False,
+        }
+    ]
+}
