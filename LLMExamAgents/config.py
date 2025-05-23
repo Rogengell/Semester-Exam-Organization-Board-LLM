1151e3618d814cc2c23a06b488c0d420d1286a7d
Rogengell <niels.rogengell@gmail.com>
2025-05-23 13:19:17 +0200

    update config

--- LLMExamAgents/config.py
+++ LLMExamAgents/config.py
@@ -2,7 +2,7 @@ LLM_CONFIG = {
     "config_list": [
         {
             "model": "llama3.1:8b",
-            "api_key": "vZTO5AQxbVq4Vzlm7eJ1jrqS3s2P3rmD",
+            "client_host": "http://host.docker.internal:11434",
             "api_type": "ollama",
             "temperature": 0.0,
             "stream": False,
